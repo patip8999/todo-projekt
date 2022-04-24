bc52e1b50eba44fd4f046eb22a33287ead8523bf
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 20:15:22 +0200

    lib

---
+++ projects/task/src/lib/task.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { TaskComponent } from './task.component';
+
+
+
+@NgModule({
+  declarations: [
+    TaskComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    TaskComponent
+  ]
+})
+export class TaskModule { }
