6312e3cc1dfa7e34bc18689a7b59fe6f94494dd8
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 22:01:29 +0200

    dto

--- projects/task/src/lib/adapters/primary/ui/addtask.component.ts
+++ projects/task/src/lib/adapters/primary/ui/addtask.component.ts
@@ -1,5 +1,12 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TaskDTO } from '../../../application/ports/secondary/task.dto';
+import { GETS_ALL_TASK_DTO, GetsAllTaskDtoPort } from '../../../application/ports/secondary/gets-all-task.dto-port';
 
 @Component({ selector: 'lib-addtask', templateUrl: './addtask.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class AddtaskComponent {
+  task$: Observable<TaskDTO[]> = this._getsAllTaskDto.getAll();
+
+  constructor(@Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort) {
+  }
 }
--- projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service-module.ts
+++ projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FirebaseTaskService } from './firebase-task.service';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ADDS_TASK_DTO } from '../../../application/ports/secondary/adds-task.dto-port';
+import { GETS_ALL_TASK_DTO } from '../../../application/ports/secondary/gets-all-task.dto-port';
 
 @NgModule({ imports: [AngularFirestoreModule],
   	declarations: [],
-  	providers: [FirebaseTaskService],
+  	providers: [FirebaseTaskService, { provide: ADDS_TASK_DTO, useExisting: FirebaseTaskService }, { provide: GETS_ALL_TASK_DTO, useExisting: FirebaseTaskService }],
   	exports: [] })
 export class FirebaseTaskServiceModule {
 }
