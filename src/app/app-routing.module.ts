5e1de77e8f268f44fee003081d3ba34a3534c4aa
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 19:41:02 +0200

    firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [{
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomePagePageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
