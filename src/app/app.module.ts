53aec5e2a78c30f936a780babdd5d5a3d4ae31ff
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 20:05:53 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY2VPDHZHRT_QkY2oXIVEKpSg0T5MwR-M",
+  authDomain: "todo-ffcf6.firebaseapp.com",
+  projectId: "todo-ffcf6",
+  storageBucket: "todo-ffcf6.appspot.com",
+  messagingSenderId: "560993699896",
+  appId: "1:560993699896:web:a04bc0a6288e52ae8a3ecd"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +21,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
