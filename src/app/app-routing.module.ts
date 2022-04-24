d9d4b55a59711d5e75aad75faab802befcd764fd
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 18:02:16 +0200

    page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagePageModule } from './pages/home-page.page-module';
+import { AddTaskPageModule } from './pages/add-task.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'home', 
+        loadChildren: () => HomePagePageModule
+      },
+  { 
+        path: 'AddTask', 
+        loadChildren: () => AddTaskPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/add-task.page.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'app-add-task-page', templateUrl: './add-task.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class AddTaskPage {
+}
