2ede7ce99168b6f135505aa97f8f2287b7518712
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-04-24 21:26:20 +0200

    add

--- src/app/pages/add-task.page-module.ts
+++ src/app/pages/add-task.page-module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AddTaskPage } from './add-task.page';
 
+import { AddtaskComponentModule } from '../../../projects/task/src/lib/adapters/primary/ui/addtask.component-module';
+
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
         {
           path: '',
           component: AddTaskPage,
         }
-      ])],
+      ]),
+  AddtaskComponentModule
+],
   	declarations: [AddTaskPage],
   	providers: [],
   	exports: [] })
