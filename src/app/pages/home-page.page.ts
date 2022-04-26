a6af4ef21ce1cad94f84438009994c5617618abe
patip8999 <patip8999@gmail.comm>
2022-04-26 22:02:16 +0200

    kk

--- src/app/pages/home-page.page.ts
+++ src/app/pages/home-page.page.ts
@@ -1,5 +1,18 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-
+import { HomePagePageModule } from './home-page.page-module';
+import { AddTaskPageModule } from './add-task.page-module';
+import { RouterModule, Routes } from '@angular/router';
 @Component({ selector: 'app-home-page-page', templateUrl: './home-page.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class HomePagePage {
 }
+
+
+const routes: Routes = [{ 
+        path: 'home', 
+        loadChildren: () => HomePagePageModule
+      },
+  { 
+        path: 'AddTask', 
+        loadChildren: () => AddTaskPageModule
+      }
+];
